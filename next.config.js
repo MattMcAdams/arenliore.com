f30ae11f259df31521429a5a9ad4b5a7621df6d1
MattMcAdams <mattmcadams@outlook.com>
2023-10-06 02:38:28 -0500

    update config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  // output: "export",
 
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   trailingSlash: true,
@@ -8,6 +8,7 @@ const nextConfig = {
   // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
   skipTrailingSlashRedirect: true,
 
+  // images: { unoptimized: true },
   // Optional: Change the output directory `out` -> `dist`
   // distDir: "dist",
 };
